3d042308a500c2d00499e47e6b9bc5cb1fb60d82
Jaes98 <marcus.jaes@gmail.com>
2023-01-31 12:49:45 +0100

    Mine første ændringer

---
+++ app.js
@@ -0,0 +1,6 @@
+document.body.innerHTML += "Hello JavaScript!";
+document.body.innerHTML += "Hello JavaScript!";
+//alert("Hello Dat.js");
+console.log("Hello Dat.js");
+console.log("Sup fool");
+console.log("This is your first JavaScript App 🎉");
